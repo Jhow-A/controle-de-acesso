4cdab51c0c93962e232c9da8735e327d55abd253
Jhonathan Araujo <jonathan.reboucas@unisys.com>
2020-01-02 11:30:59 -0300

    Adição da view Login e configuração da Bundle (Bootstrap e jQuery)

---
+++ ControleDeAcesso/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ControleDeAcesso.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            /* O código abaixo informa ao MVC para incluir todos os arquivos na pasta "Scripts" que tenham a cadeia "jquery" no arquivo, seguida por um "-" e número de versão com a extensão ".js". Também fornece a referência de "~/bundles/jquery", é assim que vamos referenciar o bundle nas views posteriormente   
+*/
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-1.9.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.min.css",
+                "~/Content/estilo.css"));
+        }
+    }
+}
--- ControleDeAcesso/Global.asax.cs
+++ ControleDeAcesso/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ControleDeAcesso.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ControleDeAcesso
@@ -13,6 +15,8 @@ namespace ControleDeAcesso
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            // Adiciona a chamada do registro das bundles
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
