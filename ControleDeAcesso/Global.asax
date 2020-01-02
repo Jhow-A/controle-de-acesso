ff51c0678df376872ee982a7a91f53219b26dfa0
Jhonathan Araujo <jonathan.reboucas@unisys.com>
2020-01-02 11:00:53 -0300

    Configuração inicial do projeto

---
+++ ControleDeAcesso/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ControleDeAcesso.MvcApplication" Language="C#" %>
---
+++ SQLQuery.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE DevMedia; 
+
+USE DevMedia; 
+
+CREATE TABLE Acesso 
+  ( 
+     id_login  INT IDENTITY NOT NULL, 
+     email     VARCHAR(100), 
+     senha     VARCHAR(100), 
+     ativo     CHAR(1), 
+     perfil    VARCHAR(15), 
+     nome      VARCHAR(30), 
+     sobrenome VARCHAR(30) 
+  ); 
+
+INSERT INTO Acesso 
+            (email, 
+             senha, 
+             ativo, 
+             perfil, 
+             nome, 
+             sobrenome) 
+VALUES      ('admin@admin.com', 
+             '123456', 
+             'S', 
+             'Administrador', 
+             'Admin', 
+             'Sistema'); 
+
+INSERT INTO Acesso 
+VALUES      ('user@user.com', 
+             '123456', 
+             'S', 
+             'Usuario', 
+             'User', 
+             'Sistema'); 
+
+SELECT * 
+FROM   Acesso; 
+
+-- Retorna o valor atual do IDETITY
+SELECT IDENT_CURRENT('acesso');
